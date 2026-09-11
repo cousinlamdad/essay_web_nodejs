f708b48a166d9e6f9f4bdcc42a401e8afc6cbee6
cousinlamdad <cousinlam@gmail.com>
2026-09-11 10:34:52 +0800

    Suppress TS error

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+/// <reference types="vite/client" />
+
+// Above comment is to suppress TS error for side-effect import of './style.css'
+// Cannot find module or type declarations for side-effect import of './style.css'.ts(2882)
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './i18n'
